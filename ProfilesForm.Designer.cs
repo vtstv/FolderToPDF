95c292f8f046e6f7f5500527c80d2cb67a31a7a7
Murr <vtsmob@gmail.com>
2024-11-23 17:59:04 +0100

    ui fix

--- ProfilesForm.Designer.cs
+++ ProfilesForm.Designer.cs
@@ -16,23 +16,25 @@
             btnSaveProfile = new Button();
             btnLoadProfile = new Button();
             btnDeleteProfile = new Button();
-
             SuspendLayout();
-
+            // 
             // lstProfiles
+            // 
             lstProfiles.FormattingEnabled = true;
             lstProfiles.Location = new Point(12, 12);
             lstProfiles.Name = "lstProfiles";
-            lstProfiles.Size = new Size(260, 199);
+            lstProfiles.Size = new Size(260, 184);
             lstProfiles.TabIndex = 0;
-
+            // 
             // txtProfileName
+            // 
             txtProfileName.Location = new Point(12, 217);
             txtProfileName.Name = "txtProfileName";
             txtProfileName.Size = new Size(260, 27);
             txtProfileName.TabIndex = 1;
-
+            // 
             // btnSaveProfile
+            // 
             btnSaveProfile.Location = new Point(12, 250);
             btnSaveProfile.Name = "btnSaveProfile";
             btnSaveProfile.Size = new Size(75, 23);
@@ -40,8 +42,9 @@
             btnSaveProfile.Text = "Save Profile";
             btnSaveProfile.UseVisualStyleBackColor = true;
             btnSaveProfile.Click += BtnSaveProfile_Click;
-
+            // 
             // btnLoadProfile
+            // 
             btnLoadProfile.Location = new Point(93, 250);
             btnLoadProfile.Name = "btnLoadProfile";
             btnLoadProfile.Size = new Size(75, 23);
@@ -49,8 +52,9 @@
             btnLoadProfile.Text = "Load Profile";
             btnLoadProfile.UseVisualStyleBackColor = true;
             btnLoadProfile.Click += BtnLoadProfile_Click;
-
+            // 
             // btnDeleteProfile
+            // 
             btnDeleteProfile.Location = new Point(174, 250);
             btnDeleteProfile.Name = "btnDeleteProfile";
             btnDeleteProfile.Size = new Size(75, 23);
@@ -58,15 +62,20 @@
             btnDeleteProfile.Text = "Delete Profile";
             btnDeleteProfile.UseVisualStyleBackColor = true;
             btnDeleteProfile.Click += BtnDeleteProfile_Click;
-
+            // 
             // ProfilesForm
+            // 
             ClientSize = new Size(284, 285);
             Controls.Add(btnDeleteProfile);
             Controls.Add(btnLoadProfile);
             Controls.Add(btnSaveProfile);
             Controls.Add(txtProfileName);
             Controls.Add(lstProfiles);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "ProfilesForm";
+            ShowInTaskbar = false;
             Text = "Profiles";
             ResumeLayout(false);
             PerformLayout();
